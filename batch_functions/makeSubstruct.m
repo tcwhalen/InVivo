3ab870558cccc902aa757adab961e0eb6634efd0
Tim C. Whalen <timcwhalen@gmail.com>
2021-03-21 22:30:40 -0400

    improved substruct readability

--- batch_functions/makeSubstruct.m
+++ batch_functions/makeSubstruct.m
@@ -3,18 +3,20 @@ function [ subdata ] = makeSubstruct( data, fnum, varargin )
 % Takes data struct (see open_data) and a vector of integers such that 
 % max(fnum) <= data.nfiles and returns a data struct as if only those files
 % had been loaded.
-% If a field of unexpected size (scalar or nfiles x 1, will display a
-% warning (e.g., byunit, or fields with text information) Include an
-% additional argument of 0 to turn off this printing
+% If a field is of unexpected size (i.e. not scalar or (nfiles,1), such as
+% a byunit field or text information), these fields will be copied in full 
+% and a warning will be printed. Include an additional argument of 0 to 
+% turn off these warnings.
+
 global verbose 
 verbose = 1;
 if ~isempty(varargin)
     verbose = varargin(1);
 end
-subdata = substructInt(data,fnum,data.nfiles);
+subdata = substructInternal(data,fnum,data.nfiles);
 end
 
-function [ data2 ] = substructInt( data, fnum, nfiles )
+function [ data2 ] = substructInternal( data, fnum, nfiles )
 fun = @(d) substructFun(d,fnum,nfiles);
 data2 = structfun(fun,data,'UniformOutput',0);
 data2.nfiles = length(fnum);
@@ -28,14 +30,12 @@ elseif size(x,2) == nfiles
     x2 = x(:,fnum);
 else % if not nfiles length vector
     if isstruct(x)
-        x2 = substructInt(x, fnum, nfiles); % reursion
+        x2 = substructInternal(x, fnum, nfiles); % reursion
     else
         if length(x) ~= 1 && verbose % if also not scalar
             disp('Warning: strut vector or cell array of unexpected length unchanged')
-            x2 = x;
-        else
-            x2 = x;
         end
+        x2 = x;
     end
 end
 end
